b1a05b464bc7bbbceba550a0ca888047903593d1
Alberto Martinez <albermh16@gmail.com>
2025-10-16 02:21:24 +0200

    Cambios en el itemPedido

--- cliente/src/componentes/zonaTienda/Pedido/ItemPedido/ItemPedido.jsx
+++ cliente/src/componentes/zonaTienda/Pedido/ItemPedido/ItemPedido.jsx
@@ -1,51 +1,91 @@
 import './ItemPedido.css'
+import useGlobalState from '../../../../globalState/stateGlobal';
 
 function ItemPedido() {
 
+  const {pedido, setPedido} = useGlobalState();
+
+  if(!pedido || !pedido.itemsProductos){
+    return <div>No hay productos en el pedido</div>
+  }
+
 
     return (
-        <div className="card mb-3" >
-            <div className="row g-0">
+      <>
+        {pedido.itemsProductos.map( (item, index) =>
+          <div key={index}  className="card mb-3" >
+              <div className="row g-0">
 
-                <div className="col-md-3">
-                    <span className='trash-icon'><i class="fa-solid fa-trash fa-lg"></i></span>
-                    <img src='' style={{height:'180px', width:'180px'}} className="img-fluid rounded-start" alt='imagen producto'/>
-                </div>
+                  <div className="col-md-3">
+                      <span className='trash-icon'><i class="fa-solid fa-trash fa-lg"></i></span>
+                      <img src={item.producto.Imagenes?.[index] ?? ""} style={{height:'180px', width:'180px'}} className="img-fluid rounded-start" alt={item.producto.Nombre}/>
+                  </div>
 
-                <div className="col-md-6">
-                    <div className="card-body">
-                        <h5 className="card-title">nombre producto   formato select - sabor selecc</h5>
-                        <p className="card-text">Consumo preferente: ......</p>
-                        <div className='d-flex flex-row justify-content-between align-items-center'>
-                            <button type='button' className='btn btn-sm btn-hsn'>AUMENTA A .../AÑADE OTRO SABOR</button>
-                            <div>
-                                <span style={{color:'#000', fontSize:'1.5em', fontWeight:'bold'}}>0.00 €</span>
-                                <span style={{color:'#999',textDecoration:'line-through', fontSize:'1.2em', fontWeight:'bold'}}>0.00 €</span>
-                                <span className='descuento'>-0%</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                  <div className="col-md-6">
+                      <div className="card-body">
+                          <h5 className="card-title">
+                            {
+                              `${item.producto.Nombre} - ${item.producto.Sabores[index]}`
+                            }
+                          </h5>
+                          <p className="card-text">Consumo preferente: {new Date().toLocaleDateString("es-ES")}</p>
+                          <div className='d-flex flex-row justify-content-between align-items-center' >
+                              <button type='button' 
+                              className='btn btn-sm btn-hsn' 
+                              onClick={() => item.producto.Sabores}>
+                              Cambia de sabor</button>
+                              <div style={{display:"flex" , alignItems:"center"}}>
+                                  
+                                  <span style={{color:'#000', fontSize:'1.2em', fontWeight:'bold', marginLeft:'10px', whiteSpace: "nowrap"}}>
+                                      {(item.producto.Precio * (1- item.producto.Oferta / 100) * item.cantidad).toFixed(2)} €
+                                  </span>
+                                  {item.producto.Oferta > 0 && (
+                                    <>
+                                      <span style={{color:'#999',textDecoration:'line-through', fontSize:'1.2em', fontWeight:'bold', marginLeft:'10px', whiteSpace: "nowrap"}}>
+                                        {(item.producto.Precio * item.cantidad).toFixed(2)} €
+                                      </span>
+                                      <span className='descuento' style={{marginLeft:10}}>-{item.producto.Oferta}%</span>
+                                    </>
+                                  )}
+                                  
+                              </div>
+                          </div>
+                      </div>
+                  </div>
 
-                <div className="col-md-3">
-                    <div className='d-flex flex-column justify-content-end align-items-center'>
-                        <div className='d-flex flex-row justify-content-center align-items-center mb-2'>
-                            <span style={{color:'#000', fontSize:'24px', fontWeight:'bold'}}>0.00 €</span>
-                            <select className="form-select form-select-sm ms-2" aria-label=".form-select-sm example" style={{width:'100px', height:'30px', fontSize:'12px', borderRadius:'5px'}}>
+                  <div className="col-md-3">
+                      <div className='d-flex flex-column justify-content-end align-items-center'>
+                          <div className='d-flex flex-row justify-content-center align-items-center mb-2'>
+                              <span style={{color:'#000', fontSize:'24px', fontWeight:'bold'}}>
                                 {
-                                    [...Array(10).keys()].map( n => <option key={n} value={n+1} >{n+1} ud</option> )
-                                }
-                            </select>
-                        </div>
-                        <span class="" style={{color:'#00b22d', margin:'auto 0', fontWeight: 'bold'}}>Sin dosificador. Eres ECO        </span>
-                    <div>
-                </div>
-            
-            </div>
-                </div>
+                                  (item.producto.Cantidad * item.producto.Precio * (1 - item.producto.Oferta / 100)).toFixed(2)
+                                } €
+                              </span>
+                              <select
+                                className="form-select form-select-sm ms-2"
+                                aria-label=".form-select-sm example"
+                                style={{width:'100px', height:'30px', fontSize:'12px', borderRadius:'5px'}}
+                                onChange={ev => setPedido('modificar', { producto: item.producto, cantidad: parseInt(ev.target.value) })}
+                                value={item.cantidad}
+                              >
+                                  {
+                                      Array.from({ length: 12 }, (_, i) => i + 1).map( (cantidad, index) =>
+                                          <option key={index} value={cantidad}>{cantidad}</option>
+                                      )
+                                  }
+                              </select>
+                          </div>
+                          <span class="" style={{color:'#00b22d', margin:'auto 0', fontWeight: 'bold'}}>Sin dosificador. Eres ECO        </span>
+                      <div>
+                  </div>
+              
+              </div>
+                  </div>
 
-            </div>
-        </div>
+              </div>
+          </div>
+        )}
+      </>
     )
 }   
 export default ItemPedido
--- servidor/routes/endpointsTienda.js
+++ servidor/routes/endpointsTienda.js
@@ -5,46 +5,46 @@ const objetoRouterTienda = express.Router();
 
 
 
-objetoRouterTienda.get('/Categorias', 
-    async (req,res,next)=>{ 
-        try {
-            //en los parametros de la url viene el pathCategoria a buscar en tabla categorias de mongodb
-            //cliente REACT envia:  http://localhost:3000/api/Tienda/Categorias ? pathCat=principales
-            console.log(`parametros en URL pasados desde react: ${JSON.stringify(req.query)}`);
+objetoRouterTienda.get('/Categorias',
+  async (req, res, next) => {
+    try {
+      //en los parametros de la url viene el pathCategoria a buscar en tabla categorias de mongodb
+      //cliente REACT envia:  http://localhost:3000/api/Tienda/Categorias ? pathCat=principales
+      console.log(`parametros en URL pasados desde react: ${JSON.stringify(req.query)}`);
 
-            const pathCategoria = req.query.pathCat; //<--- si vale "principales" quiero buscar categorias raices, sino subcategorias de la categoria q me pasan
+      const pathCategoria = req.query.pathCat; //<--- si vale "principales" quiero buscar categorias raices, sino subcategorias de la categoria q me pasan
 
-            let patronBusqueda=pathCategoria==="principales" ? /^\d+$/ : new RegExp(`^${pathCategoria}-\\d+`); 
+      let patronBusqueda = pathCategoria === "principales" ? /^\d+$/ : new RegExp(`^${pathCategoria}-\\d+`);
 
-            await mongoose.connect(process.env.URI_MONGODB);
-            let categoriasCursor=mongoose.connection
-                                        .collection('categorias')
-                                        .find( { pathCategoria: patronBusqueda} );
-            
-            let categoriasArray=await categoriasCursor.toArray();
-            console.log(`categoriasArray recuperadas: ${JSON.stringify(categoriasArray)}`);
+      await mongoose.connect(process.env.URI_MONGODB);
+      let categoriasCursor = mongoose.connection
+        .collection('categorias')
+        .find({ pathCategoria: patronBusqueda });
 
-            res.status(200).send(
-                 {
-                        codigo:0,
-                        mensaje: `categorias recuperadas ok para pathCategoria: ${pathCategoria}`,
-                        categorias: categoriasArray
-                }
-            );
-        
+      let categoriasArray = await categoriasCursor.toArray();
+      console.log(`categoriasArray recuperadas: ${JSON.stringify(categoriasArray)}`);
 
-        } catch (error) {
-            console.log(`error al recuperar categorias: ${error}`);
-            res.status(200)
-                .send(
-                    {
-                         codigo:5, 
-                         mensaje:`error al recuperar categorias: ${error}`,
-                        categorias:[]
-                    }
-                );
+      res.status(200).send(
+        {
+          codigo: 0,
+          mensaje: `categorias recuperadas ok para pathCategoria: ${pathCategoria}`,
+          categorias: categoriasArray
         }
+      );
+
+
+    } catch (error) {
+      console.log(`error al recuperar categorias: ${error}`);
+      res.status(200)
+        .send(
+          {
+            codigo: 5,
+            mensaje: `error al recuperar categorias: ${error}`,
+            categorias: []
+          }
+        );
     }
+  }
 )
 
 objetoRouterTienda.get("/Productos", async (req, res) => {
@@ -70,7 +70,7 @@ objetoRouterTienda.get("/Productos", async (req, res) => {
     console.error(err);
     res.status(500).json({ codigo: -2, msg: "error en servidor", productos: [] });
   }
-  
+
 });
 
 
